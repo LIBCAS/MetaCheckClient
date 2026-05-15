eb5422cd59a0a90ba18c41631145d91a3af5c589
LukasSykora <lukas.sykora@inovatika.cz>
2026-05-15 14:34:10 +0200

    layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 import { NavBar } from './components/nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBar, RouterLink, RouterOutlet],
+  imports: [NavBar, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
