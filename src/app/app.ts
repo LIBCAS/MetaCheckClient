3f2851b6f58e2af13d6fc6905b0abe9f7271da84
Petr Kudela <kudy.samba@gmail.com>
2026-06-24 10:17:45 +0200

    app cleaning from AI, basic app code setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { NavBar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, NavBar, RouterLink, RouterOutlet],
+  imports: [MatButtonModule, NavBar, RouterLink, RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
