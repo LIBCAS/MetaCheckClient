9e682ef4145927361e3b8650f700830ff12b0d5e
Petr Kudela <kudy.samba@gmail.com>
2026-06-29 13:36:46 +0200

    batch - router link fix, import, batch czech translation, batch - img selected in grid view

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 import { NavBar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, NavBar, RouterLink, RouterOutlet, Footer],
+  imports: [MatButtonModule, NavBar, RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
